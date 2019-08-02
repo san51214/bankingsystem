36836b177258b4a23eaefb7621b763b5024ec16d
Syed <san51214@yahoo.com>
2019-08-02 03:10:04 +0100

    Added:
    - Added lombok support
    
    Modified: none
    
    Deleted: none

--- src/main/java/com/openpayd/clientstransactions/entities/Client.java
+++ src/main/java/com/openpayd/clientstransactions/entities/Client.java
@@ -1,58 +1,36 @@
 package com.openpayd.clientstransactions.entities;
 
+import lombok.Data;
+
 import javax.persistence.*;
-import java.math.BigInteger;
 import java.util.Objects;
+
 
 @Entity
 @Table(name = "client", schema = "client_transactions", catalog = "postgres")
-public class Client {
-    private BigInteger id;
-    private String name;
-    private String surname;
-
-    @Id
-    @Column(name = "id")
-    public BigInteger getId() {
-        return id;
-    }
-
-    public void setId(BigInteger id) {
-        this.id = id;
-    }
-
+@Data
+public class Client extends BaseIdentity{
     @Basic
     @Column(name = "name")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    private String name;
     @Basic
     @Column(name = "surname")
-    public String getSurname() {
-        return surname;
-    }
+    private String surname;
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+    public Client() {}
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Client client = (Client) o;
-        return Objects.equals(id, client.id) &&
+        return Objects.equals(getId(), client.getId()) &&
                 Objects.equals(name, client.name) &&
                 Objects.equals(surname, client.surname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, surname);
+        return Objects.hash(getId(), name, surname);
     }
 }
